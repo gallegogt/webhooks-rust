cb6ce81100bdf372b5f2414044b6568fb8a3979a
Yolier Galan Tasse <gallegogt@gmail.com>
2017-08-04 17:38:15 -0400

    Primera version heroku webhooks rust

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+use rocket::config::{Config, Environment};
+use std::env;
+
+/// Look up our server port number in PORT, for compatibility with Heroku.
+fn get_server_port() -> u16 {
+    env::var("PORT")
+        .ok().and_then(|p| p.parse().ok())
+        .unwrap_or(8000)
+}
+
+#[get("/webhooks")]
+fn webhooks() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    let config = Config::build(Environment::active().unwrap())
+        .port(get_server_port())
+        .unwrap();
+
+    rocket::custom(config, true)
+        .mount("/", routes![webhooks])
+        .launch();
+}
